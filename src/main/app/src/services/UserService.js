b89fc9a904e69400a15589b1a3a06f6363bc97fa
Lylio <cxz@gmx.com>
2022-06-14 22:06:59 +0100

    axios update

--- src/main/app/src/services/UserService.js
+++ src/main/app/src/services/UserService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const USER_API_BASE_URL = "http://localhost:8080/api/contacts";
+const USER_API_BASE_URL = "https://cookie-contacts.herokuapp.com/api/contacts";
 
 class UserService {
 
